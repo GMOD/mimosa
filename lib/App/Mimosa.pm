95611ecb6a76cb21850b54b1025196fea4255225
Jonathan "Duke" Leto <jonathan@leto.net>
2011-02-10 14:03:40 -0800

    Fix bug where sequence was not being grabbed correctly

--- lib/App/Mimosa.pm
+++ lib/App/Mimosa.pm
@@ -21,10 +21,10 @@ get '/jobs' => sub {
 post '/submit' => sub {
     # TODO: VALIDATION!
     # parse posted info
-    my ($input_fh, $input_filename) = tempfile();
-    my ($output_fh, $output_filename) = tempfile();
+    my ($input_fh, $input_filename) = tempfile( CLEANUP => 0 );
+    my ($output_fh, $output_filename) = tempfile( CLEANUP => 0 );
 
-    print $input_fh params->{sequence_input};
+    print $input_fh params->{sequence};
     close $input_fh;
 
     my $j = App::Mimosa::Job->new(
