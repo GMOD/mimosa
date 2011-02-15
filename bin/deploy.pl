4880b6a0a07d40a0d489ca8b2c0ea60e1a026f29
Jonathan "Duke" Leto <jonathan@leto.net>
2011-02-15 11:53:01 -0800

    Correctly populate Mimosa::SequenceSet with S. peruvianum

--- bin/deploy.pl
+++ bin/deploy.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 use YAML qw/LoadFile/;
 use Bio::Chado::Schema;
+use Test::More;
 
 my $config = shift;
 die "need configuration file" unless $config;
@@ -13,7 +14,8 @@ my $schema = Bio::Chado::Schema->connect( $conf->{dsn} );
 diag "Deploying Mimosa Schema";
 $schema->deploy;
 
+diag "Populating default Mimosa Schema";
 $schema->populate('Bio::Chado::Schema::Mimosa::SequenceSet', [
     [qw/shortname title description alphabet source_spec lookup_spec info_url update_interval is_public/],
-    ['solanum_peruvianum_mrna', 'Solanum peruvianum SGN mRNA sequences', 'nucleotide', '', '', 30       ]
+    ['solanum_peruvianum_mrna', 'Solanum peruvianum SGN mRNA sequences', 'mRNA sequences for S. peruvianum', 'nucleotide', '', '', ,'', 30, 1    ],
 ]);
