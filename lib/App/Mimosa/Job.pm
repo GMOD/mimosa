3aa6e8d0a01cbecede5cdc0544832c0f99e3b906
Jonathan "Duke" Leto <jonathan@leto.net>
2011-02-18 16:44:09 -0800

    Create a Program subtype to validate that we get a kosher program name

--- lib/App/Mimosa/Job.pm
+++ lib/App/Mimosa/Job.pm
@@ -2,13 +2,20 @@ package App::Mimosa::Job;
 use Moose;
 use Bio::SeqIO;
 use autodie qw/:all/;
+use Moose::Util::TypeConstraints;
+
 
 # Good breakdown of commandline flags
 # http://www.molbiol.ox.ac.uk/analysis_tools/BLAST/BLAST_blastall.shtml
+subtype 'Program'
+             => as Str
+             => where {
+                    /^blast(n|p|all)$/;
+                };
 
 # validate!
 has program => (
-    isa     => 'Str',
+    isa     => 'Program',
     is      => 'rw',
     default => 'blastn',
 );
