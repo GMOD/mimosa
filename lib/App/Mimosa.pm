f42a9077ae01dc49b9a8437ad51170163ce7afc0
Robert Buels <rmb32@cornell.edu>
2011-03-07 17:15:21 -0500

    remove static-simple extra config, no longer necessary

--- lib/App/Mimosa.pm
+++ lib/App/Mimosa.pm
@@ -29,9 +29,6 @@ __PACKAGE__->config(
     name => 'App::Mimosa',
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
-    static => {
-        dirs => [ qw/static/ ],
-    },
 );
 
 # Start the application
