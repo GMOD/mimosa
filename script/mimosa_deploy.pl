48b06bca04d22ada6b4552dbc6a6ca9bf99f89bc
Jonathan "Duke" Leto <jonathan@leto.net>
2011-03-30 11:17:32 -0700

    Add some Acaryochloris marina protein data
    This will be useful as test data, as well as being in our default
    deploy of sequence sets. It satifisies our need for a protein
    sequence set. Before this, we had only nucleotide sequence sets.
    This will allow testing the filtering of sequence sets based on
    which BLAST program is chosen.

--- script/mimosa_deploy.pl
+++ script/mimosa_deploy.pl
@@ -15,6 +15,7 @@ $schema->deploy;
 diag "Populating default Mimosa Schema";
 $schema->populate('Mimosa::SequenceSet', [
     [qw/shortname title description alphabet source_spec lookup_spec info_url update_interval is_public/],
+    ['acaryochloris_marina', 'Acaryochloris marina hypothetical proteins', 'Acaryochloris marina hypothetical proteins in CP000846', 'protein', '', '', ,'', 30, 1    ],
     ['solanum_peruvianum_mrna', 'Solanum peruvianum SGN mRNA sequences', 'mRNA sequences for S. peruvianum', 'nucleotide', '', '', ,'', 30, 1    ],
-    ['prunus_necrotus_coat', 'Prunus necrotic ringspot virus', 'Necrotic ringspot virus CP gene for coat protein, isolate SK25, genomic RNA', 'nucleotide', '', '', ,'', 30, 1    ],
+    ['prunus_necrotus_coat', 'Prunus necrotic ringspot virus coat', 'Necrotic ringspot virus CP gene for coat protein, isolate SK25, genomic RNA', 'nucleotide', '', '', ,'', 30, 1    ],
 ]);
