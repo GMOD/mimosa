70b3bf68a1c5272851afc8012a59d61c3090a0ea
Jonathan "Duke" Leto <jonathan@leto.net>
2011-03-30 19:01:14 -0700

    Fill in the source_spec for the A. marina sequence set

--- script/mimosa_deploy.pl
+++ script/mimosa_deploy.pl
@@ -15,7 +15,7 @@ $schema->deploy;
 diag "Populating default Mimosa Schema";
 $schema->populate('Mimosa::SequenceSet', [
     [qw/shortname title description alphabet source_spec lookup_spec info_url update_interval is_public/],
-    ['acaryochloris_marina', 'Acaryochloris marina hypothetical proteins', 'Acaryochloris marina hypothetical proteins in CP000846', 'protein', '', '', ,'', 30, 1    ],
+    ['acaryochloris_marina', 'Acaryochloris marina hypothetical proteins', 'Acaryochloris marina hypothetical proteins in CP000846', 'protein', 'ftp://ftp.ncbi.nlm.nih.gov/genbank/genomes/Bacteria/Acaryochloris_marina_MBIC11017_uid12997/CP000846.faa', '', ,'', 30, 1    ],
     ['solanum_peruvianum_mrna', 'Solanum peruvianum SGN mRNA sequences', 'mRNA sequences for S. peruvianum', 'nucleotide', '', '', ,'', 30, 1    ],
     ['prunus_necrotus_coat', 'Prunus necrotic ringspot virus coat', 'Necrotic ringspot virus CP gene for coat protein, isolate SK25, genomic RNA', 'nucleotide', '', '', ,'', 30, 1    ],
 ]);
