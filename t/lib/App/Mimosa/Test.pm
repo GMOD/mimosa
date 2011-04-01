fe420bcc22d770e9227c79aeeade51bf1aa68e9e
Jonathan "Duke" Leto <jonathan@leto.net>
2011-03-31 17:18:35 -0700

    Fix test suite bug by deploying in App::Mimosa::Test if we don't already have a test database

--- t/lib/App/Mimosa/Test.pm
+++ t/lib/App/Mimosa/Test.pm
@@ -1,6 +1,7 @@
 package App::Mimosa::Test;
 use strict;
 use warnings;
+use autodie qw/:all/;
 
 use base 'Exporter';
 our @EXPORT = (
@@ -22,12 +23,19 @@ BEGIN {
     delete $ENV{CATALYST_SERVER};
     delete $ENV{APP_MIMOSA_SERVER};
     $ENV{CATALYST_CONFIG_LOCAL_SUFFIX} = 'testing';
+
+    # TODO: Look this up in app_mimosa_testing.conf
+    unless (-e "mimosa_test.db") {
+        system("$^X -Ilib ./script/mimosa_deploy.pl app_mimosa_testing.conf");
+
+    }
 }
 
 # load the app, grab the context object so we can use it for configuration
 use Catalyst::Test 'App::Mimosa';
 my ( undef, $c ) = ctx_request('/nonexistent_url_for_t_lib_app_mimosa_test');
 sub app { $c }
+
 
 1;
 
