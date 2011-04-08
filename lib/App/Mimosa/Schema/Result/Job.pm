1a213fd1a0a1086e1abe2f07972b8b597a021521
Jonathan "Duke" Leto <jonathan@leto.net>
2011-04-07 17:30:21 -0700

    Add the beginning of a dbix result class to keep track of Mimosa jobs

---
+++ lib/App/Mimosa/Schema/Result/Job.pm
@@ -0,0 +1,53 @@
+package App::Mimosa::Schema::Result::Job;
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 NAME
+
+App::Mimosa::Result::Job - Mimosa Job
+
+=head1 COLUMNS
+
+=cut
+
+__PACKAGE__->table("mimosa_job");
+
+=head2 mimosa_job_id
+
+Auto-incrementing surrogate primary key.
+
+=head2 sha1
+
+SHA1 hash of the job parameters. This is used to identify duplicate job requests, or
+requests for jobs that are already running.
+
+Not null, varchar(40).
+
+=head2 finished
+
+Boolean value describing whether this job is finished.
+
+=cut
+
+__PACKAGE__->add_columns(
+
+  "mimosa_job_id",
+  {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "mimosa_job_mimosa_job_id_seq",
+  },
+
+  'sha1',
+  { data_type => "varchar", is_nullable => 0, size => 40 },
+
+  'finished',
+  { data_type => "bool", is_nullable => 0, default => 0 },
+
+);
+
+__PACKAGE__->set_primary_key("mimosa_job_id");
+__PACKAGE__->add_unique_constraint("mimosa_job_c1", ['sha1'] );
