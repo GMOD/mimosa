180ecb422f4285471b6a012d3b86264fad6e3b56
Jonathan "Duke" Leto <jonathan@leto.net>
2011-12-09 12:16:10 -0800

    Prevent users (on the client-side) from submitting empty alignment queries, closes #114
    We now give the user a friendly alert window when they attempt to submit
    an empty query sequence, which saves the server from having to do it.
    Logic for checking this server-side will still be kept for defensive
    reasons.

--- root/js/mimosa-grid.js
+++ root/js/mimosa-grid.js
@@ -152,13 +152,15 @@ Ext.onReady(function(){
 
     jQuery("#search_description").keyup(filter);
 
-    jQuery("#align_button").click(function() {
+    jQuery("#main_input_form").submit(function() {
         var seq = jQuery("#sequence_input").val();
-        if (seq.length()) {
+
+        if (seq) {
+            return true;
         } else {
             alert("Sequence input is empty!");
-            return false;
         }
+        return false;
     });
 
     jQuery("#speruvianum").click(function() {
